99ace5aedf7b1b689bd6d771e788bb69370f63b4
Martin Weindel <martin.weindel@sap.com>
2024-05-14 18:44:51 +0200

    fine tuning

--- cmd/seed_server/app/firewall.go
+++ cmd/seed_server/app/firewall.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/coreos/go-iptables/iptables"
@@ -19,7 +20,7 @@ func firewallCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "firewall",
 		Short: "firewall",
-		Args:  cobra.NoArgs,
+		Args:  cobra.ArbitraryArgs,
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			log, err := utils.InitRun(cmd, Name)
 			if err != nil {
@@ -38,19 +39,26 @@ func firewallCommand() *cobra.Command {
 }
 
 func runFirewallCommand(_ context.Context, _ context.CancelFunc, log logr.Logger, device, mode string) error {
-	iptable, err := iptables.New()
+	os.Setenv("PATH", "/sbin")
+	iptable4, err := iptables.New(iptables.IPFamily(iptables.ProtocolIPv4))
+	if err != nil {
+		return err
+	}
+	iptable6, err := iptables.New(iptables.IPFamily(iptables.ProtocolIPv6))
 	if err != nil {
 		return err
 	}
 
-	var op func(table, chain string, spec ...string) error
+	var op4, op6 func(table, chain string, spec ...string) error
 	var opName string
 	switch mode {
 	case "up":
-		op = iptable.Append
+		op4 = iptable4.Append
+		op6 = iptable6.Append
 		opName = "-A"
 	case "down":
-		op = iptable.DeleteIfExists
+		op4 = iptable4.DeleteIfExists
+		op6 = iptable6.DeleteIfExists
 		opName = "-D"
 	default:
 		return errors.New("mode flag must be down or up")
@@ -60,7 +68,10 @@ func runFirewallCommand(_ context.Context, _ context.CancelFunc, log logr.Logger
 		{"-m", "state", "--state", "RELATED,ESTABLISHED", "-i", device, "-j", "ACCEPT"},
 		{"-i", device, "-j", "DROP"},
 	} {
-		if err := op("raw", "INPUT", spec...); err != nil {
+		if err := op4("filter", "INPUT", spec...); err != nil {
+			return err
+		}
+		if err := op6("filter", "INPUT", spec...); err != nil {
 			return err
 		}
 		log.Info(fmt.Sprintf("iptables %s INPUT %s", opName, strings.Join(spec, " ")))
